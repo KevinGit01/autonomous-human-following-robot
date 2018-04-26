d58391419034c95ff8892b79771624a3cb3b00c7
Kaiyang <kaiyangzheng2019@u.northwestern.edu>
2018-04-26 15:34:11 -0500

    bluetooth working

--- bluetooth/main.c
+++ bluetooth/main.c
@@ -4,24 +4,57 @@
 
 
 #define BUF_SIZE 200
+#define NU32_SYS_FREQ 80000000ul
+#define MAX_MESSAGE_LENGTH 10
+
 
 int main(){
   char buffer[BUF_SIZE];
-  NU32_Startup();
 
+  NU32_Startup();
   NU32_LED1 = 1;
   NU32_LED2 = 1;
 
-  __builtin_disable_interrupts();
-  pwmInit();
-  __builtin_enable_interrupts();
-  TRISBbits.TRISB1 = 1;
-  //dir(1);
+  U1MODEbits.BRGH = 0;
+//  U3BRG = (float)((NU32_SYS_FREQ / 38400) / 16) - 1;
+  U1BRG = 520;
+
+  // configure TX & RX pins
+  U1STAbits.UTXEN = 1;
+  U1STAbits.URXEN = 1;
+
+// turn on UART1
+  U1MODEbits.ON = 1;
+  char message[MAX_MESSAGE_LENGTH];
+  TRISBbits.TRISB0 = 0;
 
   while(1){
-    if(PORTBbits.RB1 == 0){
-    dir(0);
+      unsigned char data = 0;
+      if(U1STAbits.URXDA) { // poll to see if there is data to read in RX FIFO
+          data = U1RXREG;
+        //  NU32_LED1 = 0;
+          if(data == 0x0f){
+          NU32_LED2 = 0;
+          }
+          if(data == 0xff){
+          LATBbits.LATB0 = 1;
+          }
+          if(data == 0x11){
+          LATBbits.LATB0 = 0;
+          NU32_LED2 = 1;
+          }
+
+          /*else if(data = 0xff){
+          NU32_LED1 = 0;
+          }else if(data = 0x11){
+          NU32_LED1 = 1;
+          NU32_LED2 = 1;
+        }*/
+      }
+
+
+
     }
-  }
+
   return 0;
 }
